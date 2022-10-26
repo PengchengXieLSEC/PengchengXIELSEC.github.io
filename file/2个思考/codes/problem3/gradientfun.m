87c6f51bc7f50b25c83bf263790419a0764d94bb
Pengcheng-OPT <pengcheng-opt@Pengcheng-OPTdeMacBook-Pro.local>
2022-10-27 00:47:27 +0800

    First Commit

--- file/2个思考/codes/problem3/gradientfun.m
+++
@@ -1,9 +0,0 @@
-%gradient
-function gradent_omegaandc=gradientfun(n,m,omegaandc,a,b)  
-gradent_omegaandc = zeros(n+1,1);
-    for i=1:m
-        aa = exp(-b(i)*(omegaandc'*a(:,i)));
-        gradent_omegaandc = gradent_omegaandc +...
-            1/m*(aa)/(1+aa)*(-b(i).*a(:,i));
-    end
-end
--- file/2个思考/codes/problem3/targetfunction.m
+++
@@ -1,8 +0,0 @@
-%Function value
-function f=targetfunction(n,m,omegaandc,a,b)
-f=0;
-   for i=1:m
-       aa = exp(-b(i)*(omegaandc'*a(:,i)));
-        f = f+1/m*log(1+aa);       
-   end
-end
